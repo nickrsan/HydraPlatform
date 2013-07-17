396fa2185ed653fb4ee628bfcc90f07c6fcab560
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-07-17 16:52:18 +0000

    Utility file for the DB interface.
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@10 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

---
+++ HydraServer/trunk/python/db/util.py
@@ -0,0 +1,33 @@
+import datetime
+import mysql.connector
+
+def connect(db_name='hydra_initial'):
+	cnx = mysql.connector.connect(user='root', database=db_name)
+	return cnx
+
+def load_csv(cnx, cursor, filepath):
+
+	sql = "insert into %(table_name)s (%(col_names)s) values (%(values)s)"
+
+	filename = filepath.split('/')[-1]
+
+	with open(filepath, 'r') as f:
+		col_names = f.readlines[0]
+		for w in f.readlines()[1:]:
+			w = w.strip()
+			entry = w.split(',')
+			params = dict(
+				table_name = filename,
+				col_names = col_names,
+				values    = entry,
+			)
+
+		dt = datetime.datetime.strptime("%s %s"%(entry[0], entry[1]), "%d/%m/%Y %H:%M:%S")
+
+		params['time'] = dt
+
+		print params	
+		cursor.execute(sql, params)
+		cnx.commit()
+		cursor.close()
+		cnx.close()
