e54ad4c2e95de6cbe6dc875a769a659253ebbcdf
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-07-18 13:05:45 +0000

    Add logging to hydra lib
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@15 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

---
+++ HydraLib/trunk/HydraLib/hydra_logging.py
@@ -0,0 +1,11 @@
+import logging
+
+def init(level="DEBUG"):
+	print dir(logging)
+	logging.addLevelName( logging.INFO, "\033[0;m%s\033[0;m" % logging.getLevelName(logging.INFO))
+	logging.addLevelName( logging.DEBUG, "\033[0;32m%s\033[0;32m" % logging.getLevelName(logging.DEBUG))
+	logging.addLevelName( logging.WARNING, "\033[0;33m%s\033[0;33m" % logging.getLevelName(logging.WARNING))
+	logging.addLevelName( logging.ERROR, "\033[0;31m%s\033[0;31m" % logging.getLevelName(logging.ERROR))
+	logging.addLevelName( logging.CRITICAL, "\033[0;35m%s\033[0;35m" % logging.getLevelName(logging.CRITICAL))
+
+	logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=level)
