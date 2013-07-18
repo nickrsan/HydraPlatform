827d4bb8d5c381b7de40f417c48cf67856a5e66d
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-07-18 17:09:36 +0000

    Loading functionality to HydraIface and split tests out into more sensible structure.
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@18 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraServer/trunk/python/db/HydraIface.py
+++ HydraServer/trunk/python/db/HydraIface.py
@@ -4,151 +4,159 @@ from HydraLib.HydraException import DBException
 
 global CNX
 CNX = None
-global cursor
-cursor = None
 
 class IfaceBase(object):
-	def __init__(self, class_name):
-		logging.info("Initialising")
-		global CNX
-		global cursor
-		if CNX is None:
-			CNX = util.connect()
-			cursor = CNX.cursor()
-		self.db = IfaceDB(class_name)
+    def __init__(self, class_name):
+        logging.info("Initialising")
+        global CNX
+        if CNX is None:
+            CNX = util.connect()
+        self.db = IfaceDB(class_name)
 
-		self.in_db = False
+        self.in_db = False
 
-	def load(self):
-		self.db.load()
+    def load(self):
+        self.db.load()
 
-	def commit(self):
-		CNX.commit()
-	
-	def delete(self):
-		if self.in_db:
-			self.db.delete()
+    def commit(self):
+        CNX.commit()
 
-	def save(self):
+    def delete(self):
+        if self.in_db:
+            self.db.delete()
 
-		if self.in_db is True:
-			if self.db.has_changed is True:
-				self.db.update()
-			else:
-				logging.debug("No changes to %s, not continuing", self.db.table_name)
-		else:
-			self.db.insert()
-			self.in_db = True
+    def save(self):
+
+        if self.in_db is True:
+            if self.db.has_changed is True:
+                self.db.update()
+            else:
+                logging.debug("No changes to %s, not continuing", self.db.table_name)
+        else:
+            self.db.insert()
+            self.in_db = True
 
 class IfaceDB(object):
 
-	def __init__(self, class_name):
-		self.db_attrs = []
-		self.pk_attrs = []
-		self.nullable_attrs = []
-		self.attr_types = {}
+    def __init__(self, class_name):
+        self.db_attrs = []
+        self.pk_attrs = []
+        self.nullable_attrs = []
+        self.attr_types = {}
 
-		#This indicates whether any values in this table have changed.
-		self.has_changed = False
+        self.cursor = CNX.cursor()
 
-		#this turns 'Project' into 'tProject' so it can identify the table
-		self.table_name = "t%s"%class_name
+        #This indicates whether any values in this table have changed.
+        self.has_changed = False
 
-		cursor.execute('desc %s' % self.table_name)
+        #this turns 'Project' into 'tProject' so it can identify the table
+        self.table_name = "t%s"%class_name
 
-		table_desc = cursor.fetchall()
+        self.cursor.execute('desc %s' % self.table_name)
 
-		logging.debug("Table desc: %s", table_desc)
+        table_desc = self.cursor.fetchall()
 
-		for col_desc in table_desc:
-			col_name = col_desc[0]
+        logging.debug("Table desc: %s", table_desc)
 
-			setattr(self, col_name, col_desc[4])
-			self.db_attrs.append(col_name)
+        for col_desc in table_desc:
+            col_name = col_desc[0]
 
-			self.attr_types[col_name] = col_desc[1]
+            setattr(self, col_name, col_desc[4])
+            self.db_attrs.append(col_name)
 
-			if col_desc[2] == 'YES':
-				self.nullable_attrs.append(col_name)
+            self.attr_types[col_name] = col_desc[1]
 
-			if col_desc[3] == 'PRI':
-				self.pk_attrs.append(col_name)
+            if col_desc[2] == 'YES':
+                self.nullable_attrs.append(col_name)
 
-			if col_desc[5] == 'auto_increment':
-				self.seq = col_name
+            if col_desc[3] == 'PRI':
+                self.pk_attrs.append(col_name)
 
-	def __setattr__(self, name, value):
-		if name != 'db_attrs' and name in self.db_attrs:
-			self.has_changed = True
+            if col_desc[5] == 'auto_increment':
+                self.seq = col_name
 
-		super(IfaceDB, self).__setattr__(name, value
+    def __setattr__(self, name, value):
+        if name != 'db_attrs' and name in self.db_attrs:
+            self.has_changed = True
 
-	def insert(self):
-		#A function to return 'null' if the inputted value is None. Otherwise return the inputted value.
-		base_insert = "insert into %(table)s (%(cols)s) values (%(vals)s);"
-		complete_insert = base_insert % dict(
-			table = self.table_name,
-			cols  = ",".join([n for n in self.db_attrs]),
-			vals  = ",".join([self.get_val(n) for n in self.db_attrs]),
-		)
+        super(IfaceDB, self).__setattr__(name, value)
 
-		logging.debug("Running insert: %s", complete_insert)
-		old_seq = cursor.lastrowid
-		
-		cursor.execute(complete_insert)
-		
-		if old_seq is None or old_seq != cursor.lastrowid:
-			setattr(self, self.seq, cursor.lastrowid)
+    def insert(self):
+        #A function to return 'null' if the inputted value is None. Otherwise return the inputted value.
+        base_insert = "insert into %(table)s (%(cols)s) values (%(vals)s);"
+        complete_insert = base_insert % dict(
+            table = self.table_name,
+            cols  = ",".join([n for n in self.db_attrs]),
+            vals  = ",".join([self.get_val(n) for n in self.db_attrs]),
+        )
 
-	def update(self):
-		#A function to return 'null' if the inputted value is None. Otherwise return the inputted value.
+        logging.debug("Running insert: %s", complete_insert)
+        old_seq = self.cursor.lastrowid
+        
+        self.cursor.execute(complete_insert)
+        
+        if old_seq is None or old_seq != self.cursor.lastrowid:
+            setattr(self, self.seq, self.cursor.lastrowid)
 
-		base_update = "update %(table)s set %(sets)s where %(pk)s;"
-		complete_update = base_update % dict(
-			table = self.table_name,
-			sets  = ",".join(["%s = %s"%(n, self.get_val(n)) for n in self.db_attrs]),
-			pk    = " and ".join(["%s = %s"%(n, self.get_val(n)) for n in self.pk_attrs]),
-		)
-		logging.debug("Running update: %s", complete_update)
-		cursor.execute(complete_update)
+    def update(self):
+        #A function to return 'null' if the inputted value is None. Otherwise return the inputted value.
 
-	def load(self):
+        base_update = "update %(table)s set %(sets)s where %(pk)s;"
+        complete_update = base_update % dict(
+            table = self.table_name,
+            sets  = ",".join(["%s = %s"%(n, self.get_val(n)) for n in self.db_attrs]),
+            pk    = " and ".join(["%s = %s"%(n, self.get_val(n)) for n in self.pk_attrs]),
+        )
+        logging.debug("Running update: %s", complete_update)
+        self.cursor.execute(complete_update)
 
-		for pk in self.pk_attrs:
-			if getattr(self, pk) is None:
-				logging.info("Primary key is not set. Cannot load row from DB.")
-				return None
+    def load(self):
 
-		base_load = "select * from %(table_name)s where %(pk)s;"
-		complete_load = base_load % dict(
-			table_name = self.table_name,
-			pk         = " and ".join(["%s = %s"%(n, self.get_val(n)) for n in self.pk_attrs]),
-		)
-		logging.debug("Running load: %s", complete_load)
-		cursor.execute(complete_load)
-		result_row = cursor.fetchall()
-		if len(result_row) == 0:
-			raise DBException("No entry found for table")
+        for pk in self.pk_attrs:
+            if getattr(self, pk) is None:
+                logging.info("Primary key is not set. Cannot load row from DB.")
+                return None
 
-	def delete(self):
-		base_load = "delete from %(table_name)s where %(pk)s;"
-		complete_delete = base_load % dict(
-			table_name = self.table_name,
-			pk         = " and ".join(["%s = %s"%(n, self.get_val(n)) for n in self.pk_attrs]),
-		)
-		logging.debug("Running delete: %s", complete_delete)
-		cursor.execute(complete_delete)
-	
-	def get_val(self, attr):
-		val = getattr(self, attr)
-		if val is None:
-			return 'null'
-		else:
-			if self.attr_types[attr].find('varchar') >= 0:
-				return "\'%s\'"%val
-			else:
-				return str(val)
-	
+        base_load = "select * from %(table_name)s where %(pk)s;"
+        complete_load = base_load % dict(
+            table_name = self.table_name,
+            pk         = " and ".join(["%s = %s"%(n, self.get_val(n)) for n in self.pk_attrs]),
+        )
+        logging.debug("Running load: %s", complete_load)
+        self.cursor.execute(complete_load)
+        
+        row_columns  = self.cursor.column_names
+        row_data = self.cursor.fetchall()[0]#there should only be one entry
+        
+        if len(row_data) == 0:
+            raise DBException("No entry found for table")
+
+        for idx, column_name in enumerate(row_columns):
+            logging.debug("Setting column %s to %s", column_name, row_data[idx])
+            setattr(self, column_name, row_data[idx])
+
+    def delete(self):
+        base_load = "delete from %(table_name)s where %(pk)s;"
+        complete_delete = base_load % dict(
+            table_name = self.table_name,
+            pk         = " and ".join(["%s = %s"%(n, self.get_val(n)) for n in self.pk_attrs]),
+        )
+        logging.debug("Running delete: %s", complete_delete)
+        self.cursor.execute(complete_delete)
+    
+    def get_val(self, attr):
+        val = getattr(self, attr)
+        if val is None:
+            return 'null'
+        else:
+            if self.attr_types[attr].find('varchar') >= 0:
+                return "\'%s\'"%val
+            else:
+                return str(val)
+    
 class Project(IfaceBase):
-	def __init__(self):
-		IfaceBase.__init__(self, self.__class__.__name__)
+    def __init__(self, project_id = None):
+        IfaceBase.__init__(self, self.__class__.__name__)
+
+        if project_id is not None:
+            self.db.project_id = project_id
--- HydraServer/trunk/server.py
+++ HydraServer/trunk/server.py
@@ -1,22 +1,58 @@
 from db import HydraIface
 from HydraLib import hydra_logging
+from HydraLib.HydraException import DBException
+import logging
+ 
+def test_insert():
+    x = HydraIface.Project()
+    x.db.project_name = "test"
+    x.db.project_description = "test description"
+    x.save()
+    x.commit()
+
+    hydra_logging.shutdown()
+
+def test_update():
+    x = HydraIface.Project()
+    x.db.project_name = "test"
+    x.db.project_description = "test description"
+    x.save()
+    x.commit()
+
+    x.db.project_name = "test_new"
+    x.save()
+    x.commit()
+    x.load()
+    logging.debug(x.db.project_name)
+
+    hydra_logging.shutdown()
+
+def test_delete():
+    x = HydraIface.Project()
+    x.db.project_name = "test"
+    x.db.project_description = "test description"
+    x.save()
+    x.commit()
+
+    x.db.project_name = "test_new"
+    x.save()
+    x.delete()
+    x.load()
+    logging.debug(x.db.project_name)
+
+    hydra_logging.shutdown()
+
+def test_load():
+    x = HydraIface.Project(project_id=10)
+    x.load()
+
+    logging.debug(x.db.project_name)
+
+    hydra_logging.shutdown()
+
 
 if __name__ == '__main__':
-	hydra_logging.init(level='DEBUG')
-	x = HydraIface.Project()
-	x.db.project_name = "test"
-	x.db.project_description = "test description"
-	x.save()
-	x.commit()
+    hydra_logging.init(level='DEBUG')
+    test_load()
+    hydra_logging.shutdown()
 
-	x.db.project_name = "test_new"
-	x.save()
-	x.commit()
-	x.load()
-
-	x.delete()
-	x.save()
-	x.commit()
-	x.load()
-
-	hydra_logging.shutdown()
