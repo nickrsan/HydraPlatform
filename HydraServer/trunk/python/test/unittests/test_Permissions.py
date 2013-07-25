0701b01550b62a144f9f9bc2320224b5f4dd58c7
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-07-25 16:46:50 +0000

    Test for roles. Just one test to see if automatic testing works on the server
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@43 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraServer/trunk/python/test/unittests/test_Permissions.py
+++ HydraServer/trunk/python/test/unittests/test_Permissions.py
@@ -34,6 +34,39 @@ class UserTest(test_HydraIface.HydraIfaceTest):
         y = HydraIface.User(user_id=x.db.user_id)
         assert y.load() == True, "Load did not work correctly"
 
+class RoleTest(test_HydraIface.HydraIfaceTest):
+ 
+    def test_update(self):
+        x = HydraIface.Role()
+        x.db.role_name = "Unimportant users"
+        x.save()
+        x.commit()
+
+        x.db.role_name = "Important users"
+        x.save()
+        x.commit()
+        x.load()
+        assert x.db.role_name == "Important users", "Role did not update correctly"
+
+    def test_delete(self):
+        x = HydraIface.Role()
+        x.db.role_name = "Unimportant users"
+        x.save()
+        x.commit()
+
+        x.delete()
+        assert x.load() == False, "Delete did not work correctly."
+
+    def test_load(self):
+        x = HydraIface.Role()
+        x.db.role_name = "Unimportant users"
+        x.save()
+        x.commit()
+        x.load()
+
+        y = HydraIface.Role(role_id=x.db.role_id)
+        assert y.load() == True, "Load did not work correctly"
+
 
 if __name__ == "__main__":
     test_HydraIface.run() # run all tests
