e202ac492c565dbbb4343371fd1a98ac8f668356
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-08-07 13:41:18 +0000

    Some potentially useful code for when (if) we decide to dynamically generate complexmodel classes
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@73 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraServer/trunk/python/soap_server/hydra_complexmodels.py
+++ HydraServer/trunk/python/soap_server/hydra_complexmodels.py
@@ -1,8 +1,27 @@
 from spyne.model.complex import Array, ComplexModel
 from spyne.model.primitive import String, Integer, Decimal
 
+#This is on hold until we come up with a solution
+#for dynamically generating the classes below.
+#global typemap
+#typemap = {
+#    'varchar'    : String,
+#    'int'       : Integer,
+#    'double'    : Decimal,
+#    'blob'      : Array,
+#    'timestamp' : DateTime,
+#}
+#
+#def get_spyne_type(db_type_dict):
+#    type_info = []
+#    for col_name, col_type in db_type_dict.items():
+#        base_type = col_type.split('(')[0]
+#        spyne_type = typemap[base_type]
+#        type_info.append((col_name, spyne_type))
+#    return type_info
+
 class Attribute(ComplexModel):
- _type_info = [
+    _type_info = [
         ('attribute_id', Integer),
         ('attribute_name', String),
     ]
