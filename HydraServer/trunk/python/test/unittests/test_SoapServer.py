22a12f6a8d6d80b0a39e91a14f6a9ecae2f2dbea
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-08-19 09:44:10 +0000

    Fix automated tests of soap server by removing hard-coded port number
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@96 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraServer/trunk/python/test/unittests/test_SoapServer.py
+++ HydraServer/trunk/python/test/unittests/test_SoapServer.py
@@ -5,7 +5,7 @@ import logging
 import shutil
 import os
 
-from HydraLib import hydra_logging
+from HydraLib import hydra_logging, util
 
 from suds.client import Client
 from tempfile import gettempdir as tmp
@@ -24,7 +24,9 @@ class SoapServerTest(unittest.TestCase):
 
     def connect(self):
         #logging.debug("Connecting to server.")
-        url = 'http://localhost:8000?wsdl'
+        config = util.load_config()
+        port = config.getint('soap_server', 'port')
+        url = 'http://localhost:%s?wsdl'%port
         client = Client(url)
         client.set_options(cache=None)
         client.add_prefix('hyd', 'soap_server.hydra_complexmodels')
