bc6dbb34c2dc8f8f332c21aa0e35dcdfc40c30f5
philipp <philipp@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-01-29 15:16:07 +0000

    Load gamsAPI dependent on platform (closes #77).
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@242 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraPlugins/GAMSplugin/trunk/gamsAPI/__init__.py
+++ HydraPlugins/GAMSplugin/trunk/gamsAPI/__init__.py
@@ -1,1 +1,11 @@
-from lib_linux_x86_64_27 import cfgmcc, dctmcc, gamsxcc, gdxcc, gevmcc, gmdcc, gmomcc, optcc
+import sys
+import os
+
+if sys.platform == 'linux2' and 'x86_64' in os.uname():
+    from lib_linux_x86_64_27 import *
+elif sys.platform == 'linux2' and 'i686' in os.uname():
+    from lib_linux_i686_27 import *
+elif sys.platform == 'win32' or sys.platform == 'cygwin':
+    from lib_win32_27 import *
+else:
+    raise ImportError
---
+++ HydraPlugins/GAMSplugin/trunk/gamsAPI/lib_win32_27/__init__.py
@@ -0,0 +1,8 @@
+import cfgmcc
+import dctmcc
+import gamsxcc
+import gdxcc
+import gevmcc
+import gmdcc
+import gmomcc
+import optcc
