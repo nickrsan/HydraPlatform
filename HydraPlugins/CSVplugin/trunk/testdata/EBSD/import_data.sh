50c7056e7c7bbef7e824e7a27d85fa5525ee3d14
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-03-31 08:28:18 +0000

    new, larger EBSD network
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@350 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraPlugins/CSVplugin/trunk/testdata/EBSD/import_data.sh
+++ HydraPlugins/CSVplugin/trunk/testdata/EBSD/import_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m cProfile -o import_profile ../../ImportCSV.py -t EBSD_network.csv -n WRZ_nodes.csv -l WRZ_links.csv -x -m ebsd_template_v5.xml
+python -m cProfile -o import_profile ../../ImportCSV.py -t EBSD_network.csv -n WRZ_nodes.csv -l WRZ_links.csv -x -m ebsd_template.xml
