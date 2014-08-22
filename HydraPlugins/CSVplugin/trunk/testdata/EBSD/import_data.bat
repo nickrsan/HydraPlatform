3d386f1cc1260315f298a65399d5e64a743e6f52
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-08-22 11:13:59 +0000

    new server with get_data functionality
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@541 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraPlugins/CSVplugin/trunk/testdata/EBSD/import_data.bat
+++ HydraPlugins/CSVplugin/trunk/testdata/EBSD/import_data.bat
@@ -1,2 +1,2 @@
-python ../../ImportCSV/ImportCSV.py -t EBSD_network.csv -n WRZ_nodes.csv -l WRZ_links.csv -x -m ebsd_template.xml
+call %~dp0../../CSVPlugin/plugins/ImportCSV/ImportCSV.exe -t EBSD_network.csv -n WRZ_nodes.csv -l WRZ_links.csv -x -m ebsd_template.xml
 PAUSE
