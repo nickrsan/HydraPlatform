050c52a247d630ec0a6c430de88bccba7d311cee
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-08-28 10:32:49 +0000

    Add calvin template with data types
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@547 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraPlugins/CSVplugin/trunk/testdata/Calvin/import_data.sh
+++ HydraPlugins/CSVplugin/trunk/testdata/Calvin/import_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ../../ImportCSV/ImportCSV.py -t Calvin_network.csv -n Calvin_Nodes.csv Calvin_Inflow_Nodes.csv -l Calvin_Links.csv -x -m calvin_template_v10.xml
+python ../../ImportCSV/ImportCSV.py -t Calvin_network.csv -n Calvin_Nodes.csv Calvin_Inflow_Nodes.csv -l Calvin_Links.csv -x -m calvin_template.xml
