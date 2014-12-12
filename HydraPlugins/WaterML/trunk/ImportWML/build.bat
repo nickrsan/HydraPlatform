17feb92bb950077e24c0fb343ad869d0ec6ea907
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-12-12 18:31:37 +0000

    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@676 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraPlugins/WaterML/trunk/ImportWML/build.bat
+++ HydraPlugins/WaterML/trunk/ImportWML/build.bat
@@ -1,4 +1,5 @@
 pyinstaller --upx-dir=../../../upx ImportWML.spec
 copy dist\ImportWML\* ..\WaterMLPlugin\plugins\ImportWML\
 copy plugin.xml ..\WaterMLPlugin\plugins\ImportWML\plugin.xml
+rd /S /Q build
 PAUSE
