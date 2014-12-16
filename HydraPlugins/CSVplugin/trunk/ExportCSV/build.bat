1e986c0e4ff3c1703231c200f4cb744f38b85f46
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-12-16 15:59:00 +0000

    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@685 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraPlugins/CSVplugin/trunk/ExportCSV/build.bat
+++ HydraPlugins/CSVplugin/trunk/ExportCSV/build.bat
@@ -1,4 +1,6 @@
 pyinstaller --upx-dir=../../../upx ExportCSV.spec
 copy dist\ExportCSV\* ..\CSVPlugin\plugins\ExportCSV\
+mkdir ..\CSVPlugin\plugins\ExportCSV\eggs\
+xcopy dist\ExportCSV\eggs ..\CSVPlugin\plugins\ExportCSV\eggs /S
 copy plugin.xml ..\CSVPlugin\plugins\ExportCSV\plugin.xml
 PAUSE
